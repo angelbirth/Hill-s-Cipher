b0a4fcc51a09fe125fb1b7814f9d89b3793c7e67
angelbirth <ricky.ason@gmail.com>
2016-04-14 01:28:31 +0700

    invert.m

--- invert.m
+++ invert.m
@@ -13,21 +13,30 @@
 # sub_mult_row(mat, minuend,subtrahend, k): subtract k*row subtrahend from minuend
 
 
-function y = invert (mat, mod)
+function y = invert (mat, modulo)
 [r ~]=size(mat);
 temp=[mat(:,:) eye(r)];
 %step 1: forward elimination
 for i=1:r
-  temp=mult_row(temp,i,modinv(temp(i,i),mod));
-  temp=builtin("mod",temp,mod);
+  temp=mult_row(temp,i,modinv(temp(i,i),modulo));
+  temp=mod(temp,modulo);
   for j=i+1:r
     if temp(j,i)==0; continue;endif;
-    k=mat(j,i)*modinv(mat(i,i),mod);
-    k=builtin("mod",k,mod);
-    temp=builtin("mod",sub_mult_row(temp,j,i,k),mod);
+    k=temp(j,i);
+    %k=mat(j,i)*modinv(mat(i,i),modulo);
+    %k=mod(k,modulo);
+    temp=mod(sub_mult_row(temp,j,i,k),modulo);
   endfor
 endfor
 y=temp;
+
+%step 2: backward elimination
+for i=r:-1:1
+  for j=i-1:-1:1
+    k=y(j,i);
+    y=mod(sub_mult_row(y,j,i,k),modulo);
+  endfor
+endfor
 endfunction
 
 function mat=swaprow(mat,row1,row2)
